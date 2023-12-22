73f7cfe582dc0c6a5d32f8f0a55320a6d992d818
Jose <nino.capati@gmail.com>
2023-12-22 23:01:34 +0800

    starting point

--- index.ts
+++ index.ts
@@ -1,94 +1,149 @@
 
-    interface Validator {
-      validate: (value: string) => boolean;
-      message: string;
-    }
+interface Validator {
+    validate: (value: string) => boolean;
+    message: string;
+}
 
-    interface FormControl {
-      name: string;
-      id: string;
-      value: string;
-      validators: Validator[];
-      valid: boolean;
-      touched: boolean;
-    }
+interface FormControl {
+    name: string;
+    id: string;
+    value: string;
+    validators: Validator[];
+    valid: boolean;
+    touched: boolean;
+}
 
-    export default class ReactiveFormBuilder {
-      private formControls: { [key: string]: FormControl } = {};
-      private formElement: HTMLFormElement;
+export default class PaoForm {
+    private formControls: { [key: string]: FormControl } = {};
+    private formElement: HTMLFormElement;
 
-      constructor(formId: string) {
+    constructor(formId: string) {
         this.formElement = document.getElementById(formId) as HTMLFormElement;
 
         this.formElement.addEventListener('input', (event) => {
-          const inputElement = event.target as HTMLInputElement;
-          const controlName = inputElement.name;
+            const inputElement = event.target as HTMLInputElement;
+            const controlName = inputElement.name;
 
-          // Attempt to access the form control
-          const control = this.formControls[controlName];
+            // Attempt to access the form control
+            const control = this.formControls[controlName];
 
-          if (control) {
-            control.value = inputElement.value;
-            control.touched = true;
-            control.valid = this.validateControl(control);
+            if (control) {
+                control.value = inputElement.value;
+                control.touched = true;
+                control.valid = this.validateControl(control);
 
-            const errorMessageElement = document.getElementById(`${controlName}Error`);
-            if (errorMessageElement) {
-              errorMessageElement.innerHTML = this.getErrorMessages(control);
+                const errorMessageElement = document.getElementById(`${controlName}Error`);
+                if (errorMessageElement) {
+                    errorMessageElement.innerHTML = this.getErrorMessages(control);
+                }
             }
-          }
         });
-      }
+    }
 
-      control(name: string, validators: Validator[] = []): this {
+    control(name: string, validators: Validator[] = []): this {
         this.formControls[name] = {
-          name,
-          id: name.replace(/\./g, '_'),
-          value: '',
-          validators,
-          valid: true,
-          touched: false,
+            name,
+            id: name.replace(/\./g, '_'),
+            value: '',
+            validators,
+            valid: true,
+            touched: false,
         };
         return this; // Allow method chaining
-      }
+    }
 
-      addArray(arrayName: string, size = 1): this {
+    addArray(arrayName: string, size = 1): this {
         Array.from({ length: size }, (_, index) => {
-          const name = `${arrayName}[${index}]`;
-          this.formControls[name] = {
+            const name = `${arrayName}[${index}]`;
+            this.formControls[name] = {
             name,
             id: name.replace(/\./g, '_'),
             value: '',
             validators: [],
             valid: true,
             touched: false,
-          };
+            };
         });
         return this; // Allow method chaining
-      }
+    }
 
-      removeFromArray(arrayName: string): this {
+    removeFromArray(arrayName: string): this {
         if (this.formControls[arrayName]) {
-          delete this.formControls[arrayName];
+            delete this.formControls[arrayName];
         }
         return this; // Allow method chaining
-      }
+    }
 
-      build(): { controls: { [key: string]: FormControl } } {
+    build(): { controls: { [key: string]: FormControl } } {
         return {
-          controls: this.formControls,
+            controls: this.formControls,
         };
-      }
+    }
 
-      validateControl(control: FormControl): boolean {
+    validateControl(control: FormControl): boolean {
         return control.validators.every((validator) => validator.validate(control.value));
-      }
+    }
 
-      getErrorMessages(control: FormControl): string | null {
+    getErrorMessages(control: FormControl): string {
         const errorMessages = control.validators
-          .filter((validator) => !validator.validate(control.value))
-          .map((validator) => validator.message);
+            .filter((validator) => !validator.validate(control.value))
+            .map((validator) => validator.message);
 
-        return errorMessages.join('<br>') || null;
+        return errorMessages.join('<br>') || '';
+    }
+
+    getFormStatus(): boolean {
+        return Object.values(this.formControls).every((control) => control.valid);
+    }
+
+    renderFormControls() {
+
+        // Iterate over the dynamically added addresses and create input elements
+        Object.values(this.formControls).forEach((control) => {
+          const inputElement = document.getElementById(control.id) as HTMLInputElement;
+
+          // Update the control's value based on the input element's current value
+          control.value = inputElement.value;
+
+          // Update the control's 'touched' and 'valid' properties
+          control.touched = true;
+          control.valid = this.validateControl(control);
+
+          // Update the error messages
+          const errorMessageId = `${control.id}Error`;
+          const errorMessageElement = document.getElementById(errorMessageId);
+          if (errorMessageElement) {
+            errorMessageElement.innerHTML = this.getErrorMessages(control);
+          }
+
+        });
+    }
+
+    updateErrorMessages() {
+      Object.values(this.formControls).forEach((control) => {
+        const errorMessageElement = document.getElementById(`${control.id}Error`);
+        if (errorMessageElement) {
+          errorMessageElement.innerHTML = this.getErrorMessages(control);
+        }
+      });
+    }
+
+    displayFormStatus() {
+      // Update error messages for individual controls
+      this.updateErrorMessages();
+
+      // Check the overall form status
+      const formIsValid = this.getFormStatus();
+
+      // Display or use the form status as needed
+      if (formIsValid) {
+        console.log('Form is valid!');
+      } else {
+        console.log('Form has validation errors.');
       }
     }
+
+
+
+}
+
